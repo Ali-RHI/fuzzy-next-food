2ad8412a3f254d728f34604b93c99fb287d1e555
Ali Rouhi <alirhtg@gmail.com>
2024-06-18 19:00:14 +0330

    adding attributes section

--- components/templates/HomePage.js
+++ components/templates/HomePage.js
@@ -1,9 +1,11 @@
+import Attributes from '../modules/Attributes.js';
 import Banner from '../modules/Banner.js';
 import styles from './HomePage.module.css';
 function HomePage() {
 	return (
 		<div className={styles.container}>
 			<Banner />
+			<Attributes />
 		</div>
 	);
 }
