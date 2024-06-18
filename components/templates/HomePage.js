120c08d736ea2475b8e4675037a480aae1a806b7
Ali Rouhi <alirhtg@gmail.com>
2024-06-18 19:01:17 +0330

    definiton section

--- components/templates/HomePage.js
+++ components/templates/HomePage.js
@@ -1,11 +1,13 @@
 import Attributes from '../modules/Attributes.js';
 import Banner from '../modules/Banner.js';
+import Definition from '../modules/Definition.js';
 import styles from './HomePage.module.css';
 function HomePage() {
 	return (
 		<div className={styles.container}>
 			<Banner />
 			<Attributes />
+            <Definition/>
 		</div>
 	);
 }
